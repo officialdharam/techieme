e27367ba03e660aac1afb21e1f44b1a540fa1bf2
Dharmendra Prasad <officialdharam@gmail.com>
2015-05-17 19:28:52 +0530

    Create PolynomialNode.java

---
+++ polynomial/PolynomialNode.java
@@ -0,0 +1,12 @@
+package com.interview;
+
+public class PolynomialNode {
+	float coefficient;
+	int power;
+
+	public PolynomialNode(float coefficient, int power) {
+		super();
+		this.coefficient = coefficient;
+		this.power = power;
+	}
+}
