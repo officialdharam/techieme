fc976060e35e3a3067bebd9d0177585c6df24f26
Dharmendra Prasad <officialdharam@gmail.com>
2015-05-17 23:45:39 +0530

    Create LinkedNode.java

---
+++ studentenrollment/LinkedNode.java
@@ -0,0 +1,16 @@
+package com.interview;
+
+public class LinkedNode {
+
+	LinkedNode next;
+	LinkedNode previous;
+	Integer data;
+
+	public LinkedNode(LinkedNode next, LinkedNode previous, Integer data) {
+		super();
+		this.next = next;
+		this.previous = previous;
+		this.data = data;
+	}
+
+}
