9e408164351a158397c7720506d7fe2bf9d8399f
Dharmendra Prasad <officialdharam@gmail.com>
2015-11-25 00:42:22 +0530

    Create Chair.java

---
+++ designpatterns/factory/simple/Chair.java
@@ -0,0 +1,25 @@
+public class Chair implements Furniture {
+
+	String name="New Fabric Chair";
+	
+	@Override
+	public String toString() {
+		return "Chair [name=" + name + "]";
+	}
+
+	@Override
+	public void paint() {
+		System.out.println("Painting Chair");
+	}
+
+	@Override
+	public void polish() {
+		System.out.println("Polishing Chair");
+	}
+
+	@Override
+	public void box() {
+		System.out.println("Boxing Chair");
+	}
+
+}
