edec4dc4639db86d0f3e7175d4c57146115d0419
Dharmendra Prasad <officialdharam@gmail.com>
2015-11-25 00:57:22 +0530

    Create Bed.java

---
+++ designpatterns/factory/simple/Bed.java
@@ -0,0 +1,25 @@
+public class Bed implements Furniture {
+
+	String name="New Queen Size Bed";
+	
+	@Override
+	public String toString() {
+		return "Bed [name=" + name + "]";
+	}
+
+	@Override
+	public void paint() {
+		System.out.println("Painting Bed");
+	}
+
+	@Override
+	public void polish() {
+		System.out.println("Polishing Bed");
+	}
+
+	@Override
+	public void box() {
+		System.out.println("Boxing Bed");
+	}
+
+}
