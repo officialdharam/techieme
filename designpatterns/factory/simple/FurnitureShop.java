242c21469b9db838185bf2acecd677b1f279996c
Dharmendra Prasad <officialdharam@gmail.com>
2015-11-25 00:41:23 +0530

    Create FurnitureShop.java

---
+++ designpatterns/factory/simple/FurnitureShop.java
@@ -0,0 +1,14 @@
+public class FurnitureShop {
+	public Furniture orderFurniture(String type) {
+		Furniture furniture = null;
+		if ("CHAIR".equals(type))
+			furniture = new Chair();
+		else if ("TABLE".equals(type))
+			furniture = new Table();
+
+		furniture.paint();
+		furniture.polish();
+		furniture.box();
+		return furniture;
+	}
+}
