1c6e730fd2e0f45e1ef83909a3b157bccfc1b3fe
Dharmendra Prasad <officialdharam@gmail.com>
2015-11-28 22:39:11 +0530

    Delete DelhiBasedShop.java

--- designpatterns/factory/factory-method/DelhiBasedShop.java
+++
@@ -1,7 +0,0 @@
-public class DelhiBasedShop {
-	public static void main(String[] args) {
-		AbstractProducer delhiProducer = new FurnitureProducerDelhi();
-		delhiProducer.orderFurniture("CHAIR");
-		delhiProducer.orderFurniture("TABLE");
-	}
-}
