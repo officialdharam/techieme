6d05a1638ec6b9949d85287fd6b577fdb41f0087
Dharmendra Prasad <officialdharam@gmail.com>
2015-11-28 22:39:34 +0530

    Delete JayShop.java

--- designpatterns/factory/factory-method/JayShop.java
+++
@@ -1,7 +0,0 @@
-public class JayShop {
-	public static void main(String[] args) {
-		AbstractProducer defaultProducer = new FurnitureProducerDefault();
-		defaultProducer.orderFurniture("CHAIR");
-		defaultProducer.orderFurniture("TABLE");
-	}
-}
